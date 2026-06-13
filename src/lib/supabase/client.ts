d4c84bd23e9021538079f9bba805dca50915ad4e
Caruano <valtercaruano@gmail.com>
2026-06-13 12:46:21 -0300

    Update client.ts

--- src/lib/supabase/client.ts
+++ src/lib/supabase/client.ts
@@ -3,5 +3,5 @@ import { createClient } from '@supabase/supabase-js';
 const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL || '';
 const supabaseAnonKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY || '';
 
-// Exportação nomeada correta que o site todo espera
+// Exportação nomeada padrão para o projeto todo
 export const supabase = createClient(supabaseUrl, supabaseAnonKey);
